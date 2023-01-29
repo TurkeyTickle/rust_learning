a9500d7cd9b17fcbe3411a8132513651b06b444d
Nathan Lomax <nathanlomax@live.com>
2023-01-29 00:13:04 -0500

    AoC day 1 part 2

---
+++ src/advent_of_code_2022/day_3/mod.rs
@@ -0,0 +1,3 @@
+pub fn get_solution() -> u32 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod advent_of_code_2022;
 use crate::advent_of_code_2022::{self as aoc};
 
 fn main() {
-    let day_1_solution = aoc::day_1::get_solution();
-    println!("day 1: {}", day_1_solution);
+    let elf_calorie_counts = aoc::day_1::part_1::get_elf_calorie_counts();
+    println!("day 1 part 1: {}", elf_calorie_counts.iter().max().unwrap());
+
+    let day_1_2_solution = aoc::day_1::part_2::get_top_three_elf_calorie_sum(elf_calorie_counts);
+    println!("day 1 part 2: {}", day_1_2_solution);
 
     let day_2_solution = aoc::day_2::get_solution();
     println!("day 2: {}", day_2_solution);
 
+    let day_3_solution = aoc::day_3::get_solution();
+    println!("day 3: {}", day_3_solution);
 }
